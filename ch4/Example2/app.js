c9ef68303f1167fbccd2f3885123d97c99fdda53
Alex Roosma <alex.roosma@gmail.com>
2016-10-29 21:06:25 +0300

    initial JS for button

--- ch4/Example2/app.js
+++ ch4/Example2/app.js
@@ -1,7 +1,18 @@
-var main = function(){
+var main = function () {
   "use strict";
 
-  
+  $(".comment-input button").on("click", function (event){
+    console.log("Hello World!");
+  });
 };
 
 $(document).ready(main);
+
+//
+// var main = function() {
+//   "use strict";
+//
+//   window.alert("Hello World!");
+// };
+//
+// $(document).ready(main);
