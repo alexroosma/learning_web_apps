09ee754add682f64b9b2a71976de321ae5899ca1
Alex Roosma <alex.roosma@gmail.com>
2016-11-05 18:26:16 +0200

    Refactored code - addComment method

--- ch4/Example2/app.js
+++ ch4/Example2/app.js
@@ -1,34 +1,35 @@
 var main = function () {
   "use strict";
 
-  // Handles adding comments based on button click
-  $(".comment-input button").on("click", function (event){
+  var addCommentFromInputBox = function() {
     // $("<p>") is actually an object, not just a literal char String
     // using a '$' denotes that the variable is a jQuery object
     var $new_comment;
 
     // checks for empty input before adding a new <p>
     if ($(".comment-input input") !== "") {
-      $new_comment = $("<p>").text($(".comment-input input").val()) ;
+      $new_comment = $("<p>").text($(".comment-input input").val());
+
+      $new_comment.hide();
       // add HTML piece to the page
       $(".comments").append($new_comment);
+
+      $new_comment.fadeIn();
       // clear out input box
       $(".comment-input input").val("");
     }
+  };
+
+  // Handles adding comments based on button click
+  $(".comment-input button").on("click", function (event){
+      addCommentFromInputBox();
   });
 
   // Handles adding comments based on 'Enter' key press
   $(".comment-input input").on("keypress", function (event) {
-    var $new_comment;
-
     if (event.keyCode == 13) {
-      $new_comment = $("<p>").text($(".comment-input input").val()) ;
-      // add HTML piece to the page
-      $(".comments").append($new_comment);
-      // clear out input box
-      $(".comment-input input").val("");
+      addCommentFromInputBox();
     }
-
   });
 };
 
