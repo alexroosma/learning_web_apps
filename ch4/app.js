5170fb0ed4c861cb05997435c65dbc8f12a45efa
Alex Roosma <alex.roosma@gmail.com>
2016-11-12 16:39:06 +0200

    Functions calling functions

--- ch4/app.js
+++ ch4/app.js
@@ -1,7 +1,19 @@
 var main = function() {
   "use strict";
 
-  window.alert("Hello World!");
+  var $content = $("<div>Some Content Here</div>").hide();
+  var $moreContent = $("<div>More Stuff Here</div>");
+
+  $("body").append($content);
+
+
+  $content.slideDown(2000, function() {
+
+      $("body").append($moreContent);
+
+      $moreContent.fadeIn();    
+  });
+
 };
 
 $(document).ready(main);
