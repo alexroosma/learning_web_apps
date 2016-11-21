045456f4bd64b77278bbd6e53fbd26792ee49134
Alex Roosma <alex.roosma@gmail.com>
2016-11-21 20:27:02 +0200

    Added selection check logic

--- ch4_amaze/javascripts/app.js
+++ ch4_amaze/javascripts/app.js
@@ -1,31 +1,34 @@
 var main = function() {
   "use strict";
 
-  $(".tabs a:nth-child(1)").on("click", function () {
-    activateTab(1);
-    return false;   // so we don't follow the link
+  $(".tabs span").toArray().forEach(function (element) {
+    // Given the element of the array, create a click handler for it
+    // The elements are the <span>'s whose parents are <a>'s under the <div>
+    $(element).on("click", function () {
+      // the toArray funct return DOM elements, not jQuery objects
+      // so we still need to wrap them
+
+      // we'll be using the element jQuery object a few times, so we do the
+      // conversion to an object vs DOM element up front
+      var $element = $(element);
+
+      $(".tabs span").removeClass("active");
+      $element.addClass("active");
+      $("main .content").empty();
+
+      // now we determine which of the tabs was clicked on/this element
+      // click event that got triggered
+      if ($element.parent().is(":nth-child(1)")) {
+        console.log("First Tab Clicked!!");
+      } else if ($element.parent().is(":nth-child(2)")) {
+        console.log("Second Tab Clicked!!");
+      } else if ($element.parent().is(":nth-child(3)")) {
+        console.log("Third Tab Clicked!!");
+      }
+
+      return false;
+    });
   });
-
-  $(".tabs a:nth-child(2)").on("click", function () {
-    activateTab(2);
-    return false;
-  });
-
-  $(".tabs a:nth-child(3)").on("click", function () {
-    activateTab(3);
-    return false;
-  });
-};
-
-var activateTab = function(number) {
-  // make all of the tabs inactive
-  $(".tabs span").removeClass("active");
-
-  // make the applicalbe tab active
-  $(".tabs a:nth-child("+number+") span").addClass("active");
-
-  // empty the main content so it can be re-created
-  $("main .content").empty();
 };
 
 $(document).ready(main);
